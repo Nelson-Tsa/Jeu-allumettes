29b04d02b2c1217cfe3a5fc7ef18274ee3d300b1
Nelson Tsa <t.nelson@hotmail.fr>
2025-03-11 12:24:05 +0100

    ajout des noms pour les joueurs

--- script.js
+++ script.js
@@ -100,40 +100,115 @@ function VersionInput(){
     else{
     nombreDeJoueur = nbrDeJoueur;
     console.log(nombreDeJoueur);
-    inputjoueurs(nombreDeJoueur);
+    nommerInput(nombreDeJoueur);
     return nombreDeJoueur;
     }
 }
 
 function inputjoueurs(a){
-    for (let i=1; i<=a; i++){
+    for (let i=0; i<nomDesJoueurs.length; i++){
        let nbrDeInput = document.createElement("input");
         document.getElementById("players").appendChild(nbrDeInput);
-        nbrDeInput.setAttribute("placeholder", `Joueur ${i}`);
+        nbrDeInput.setAttribute("placeholder", `${nomDesJoueurs[i]}`);
         nbrDeInput.setAttribute("type", "text");
     }
     let bouton3 = document.createElement("button");
     bouton3.textContent = "Valider";
     document.getElementById("players").appendChild(bouton3);
     bouton3.addEventListener("click", jeuxInput);
+    alert(`C'est au joueur 1 de jouer`);
     nombreDeJoueur = a;
+    for (let j=1; j<=nombreDeJoueur; j++){
+        document.querySelector(`input:nth-child(${j})`).style.display = "none";
+    }
+    document.querySelector(`input:nth-child(${joueurActuel})`).style.display = "block";
     return nombreDeJoueur;
 }
 
-function jeuxInput(){
-    
-    for (let i=1; i<=nombreDeJoueur; i++){  
-        let joueur = i;
-        let choix = Number(document.querySelector(`input:nth-child(${i})`).value);
-        boiteAllumette -= choix;
-        finPartieInput(boiteAllumette, joueur);
-        console.log(joueur, choix, boiteAllumette);
-        decompteAllumettes()
-        document.querySelector(`input:nth-child(${i})`).value = "";
-        
+let nomDesJoueurs = [];
+
+function nommerInput(a){
+nombreDeJoueur = a;
+    for (let i=1; i<=a; i++){
+        let nomJoueur = prompt(`Quel est le nom du joueur ${i}`);
+        nomDesJoueurs.push(nomJoueur);
     }
-    return boiteAllumette;
+    console.log(nomDesJoueurs);
+    inputjoueurs(nombreDeJoueur);
+return nombreDeJoueur;
+
 }
+
+function valeurInput(){
+    
+
+    for (let j=joueurActuel; j<=nombreDeJoueur; j++){
+        document.querySelector(`input:nth-child(${j})`).style.display = "none";
+    }
+    let choix = Number(document.querySelector(`input:nth-child(${joueurActuel})`).value);
+    verifierInput(choix);
+}
+
+function verifierInput(choix){
+    
+   
+
+
+    if (isNaN(choix)){
+        alert("veuillez entrer un nombre");
+            document.querySelector(`input:nth-child(${joueurActuel})`).value = "";
+            document.querySelector(`input:nth-child(${joueurActuel})`).style.display = "block"
+        return ;
+    }
+        else if (choix < 1 || choix > 6){
+            alert("veuillez entrer un nombre entre 1 et 6");
+            document.querySelector(`input:nth-child(${joueurActuel})`).value = "";
+            document.querySelector(`input:nth-child(${joueurActuel})`).style.display = "block"
+        return ;
+            
+        }
+        else {
+            boiteAllumette -= choix;
+            finPartieInput(boiteAllumette, joueurActuel);
+            console.log(joueurActuel, choix, boiteAllumette);
+            decompteAllumettes()
+            document.querySelector(`input:nth-child(${joueurActuel})`).value = "";
+            joueurSuivant(joueurActuel);
+        }
+    
+}
+
+function joueurSuivant(a){
+    joueurActuel=a;
+    joueurActuel++;
+
+    if (joueurActuel <= nombreDeJoueur){
+        alert(`C'est au joueur ${joueurActuel} de jouer`);
+        document.querySelector(`input:nth-child(${joueurActuel})`).style.display = "block";
+    }
+    else if (joueurActuel === nombreDeJoueur + 1){
+        joueurActuel = 1;
+        alert(`C'est au joueur ${joueurActuel} de jouer`);
+        document.querySelector(`input:nth-child(${joueurActuel})`).style.display = "block";
+    }
+    return joueurActuel;
+
+}
+
+
+
+let joueurActuel = 1; // 1
+
+function jeuxInput(){
+    valeurInput();
+}
+
+
+
+
+
+
+
 
     function finPartieInput(a, b){
         if ( a === 0 || a < 0){
